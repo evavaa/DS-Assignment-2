3532c3fd0c51ade07884be0bae0a54f647ee8232
evavaa <evach0305@gmail.com>
2024-04-25 22:25:02 +1000

    add user login GUI

--- src/client/LoginGUI.java
+++ src/client/LoginGUI.java
@@ -1,4 +1,29 @@
 package client;
 
-public class LoginGUI {
+import javax.swing.*;
+
+public class LoginGUI extends JFrame {
+    private JLabel title;
+    private JLabel username;
+    private JTextField usernameInput;
+    private JTextField IPAddressInput;
+    private JTextField portNumberInput;
+    private JLabel IP;
+    private JLabel portNumber;
+    private JPanel Login;
+    private JButton newButton;
+    private JButton joinButton;
+
+    public LoginGUI() {
+        setContentPane(Login);
+        setTitle("Whiteboard");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new LoginGUI();
+    }
 }
